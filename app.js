0c830d4a1432d3a59aa68145326fe8b339311f32
jass0987 <127383777+jass0987@users.noreply.github.com>
2023-09-02 11:57:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// function a(){
+//     document.getElementById("virat").innerHTML="THE KING";
+// }
+// function b(){
+//     document.querySelector("#virat").innerHTML="THE BEST"
+// }
+function c(){
+    document.getElementById("msd").src="./msd.jpg";
+}
+function d(){
+    document.getElementById("msd").src="./th.jpg";
+}
